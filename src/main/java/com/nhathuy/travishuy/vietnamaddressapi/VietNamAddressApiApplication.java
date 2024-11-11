6f492ef5118e18f65f16e330d2c1940766330c3a
TravisHuy <honhathuy098@gmail.com>
2024-11-11 12:27:28 +0700

    first commit

---
+++ src/main/java/com/nhathuy/travishuy/vietnamaddressapi/VietNamAddressApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhathuy.travishuy.vietnamaddressapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietNamAddressApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VietNamAddressApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhathuy/travishuy/vietnamaddressapi/VietNamAddressApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhathuy.travishuy.vietnamaddressapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietNamAddressApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
